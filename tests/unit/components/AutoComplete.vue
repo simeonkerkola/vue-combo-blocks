a8531c8634c17e95668ad7f09860a2e0ab100eee
sssmi <simeon.kerkola@gmail.com>
2020-08-10 15:16:00 +0300

    Add tests

---
+++ tests/unit/components/AutoComplete.vue
@@ -0,0 +1,271 @@
+<template>
+  <div :id="`${id}-autocomplete`">
+    <ComboBlocks
+      :ref="id"
+      :value="value"
+      :display-attribute="displayAttribute"
+      :value-attribute="valueAttribute"
+      :controls="{
+        autocomplete: [] // Disable (CTRL / SHFT) + Space autocomplete.
+      }"
+      @change="onChange"
+      @show-list="onShowList"
+      @focus="onFocus"
+      @hover="onHover"
+    >
+      <template
+        v-slot="{
+          getInputProps,
+          getInputEventListeners,
+          selected,
+          hoveredIndex,
+          isOpen,
+          getListProps,
+          getListEventListeners,
+          getItemProps,
+          getItemEventListeners,
+          getComboboxProps
+        }"
+      >
+        <div id="combobox" v-bind="getComboboxProps()" style="width: 100%">
+          <input
+            :id="id"
+
+            v-bind="getInputProps()"
+            :placeholder="placeholder"
+            :label="label"
+            autocomplete="off"
+            single-line
+            v-on="getInputEventListeners()"
+            @input="onInput"
+          />
+
+          <ul
+            v-show="isOpen && (list.length || !miscSlotsAreEmpty())"
+            class="list"
+
+            v-bind="getListProps()"
+            v-on="getListEventListeners()"
+          >
+            <slot name="append-item"></slot>
+            <li
+              v-for="(item, index) in list"
+              :key="item[valueAttribute]"
+              class="list-item"
+
+              v-bind="getItemProps({ item, index })"
+              v-on="getItemEventListeners(item)"
+            >
+              <span :id="`${id}-suggest-item-${item[displayAttribute]}`">
+                {{ item[displayAttribute] }}
+              </span>
+            </li>
+            <slot name="prepend-item"></slot>
+          </ul>
+        </div>
+      </template>
+    </ComboBlocks>
+
+    <button v-if="cancelButton && !!value"
+    :id="`${id}-cancel`"
+   label="cancel" @click="onCancel">
+      cancel
+    </button>
+
+    <button
+      v-if="dropdownIcon"
+      :id="`${id}-dropdown`"
+      label="dropdown"
+      @click="onDropdownIconClick"
+    >
+      open
+    </button>
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-param-reassign */
+import { get } from 'lodash';
+import ComboBlocks from '../../../src/main';
+
+export default {
+  components: {
+    ComboBlocks,
+  },
+  model: {
+    prop: 'value',
+    event: 'change',
+  },
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+    list: {
+      type: [Array, Function],
+      required: true,
+    },
+    value: {
+      // Object or null
+      validator: (prop) => typeof prop === 'object',
+      default: null,
+    },
+    displayAttribute: {
+      type: String,
+      default: 'name',
+    },
+    valueAttribute: {
+      type: String,
+      default: 'id',
+    },
+    cancelButton: {
+      type: Boolean,
+      default: false,
+    },
+    dropdownIcon: {
+      type: Boolean,
+      default: false,
+    },
+    debounce: {
+      type: Number,
+      default: 0,
+    },
+    clearAfterSelect: {
+      type: Boolean,
+      default: false,
+    },
+    searchInput: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      // inputValue: ''
+    };
+  },
+  computed: {
+    autocompleteRef() {
+      return get(this.$refs, `${this.id}`);
+    },
+  },
+  watch: {
+    value(selected) {
+      // Set the input text value to the displayAttribute of the selected item.
+      if (!selected) this.autocompleteRef.clearSelection();
+    },
+  },
+  mounted() {
+    if (this.value) this.autocompleteRef.select(this.value);
+  },
+
+  methods: {
+    onHover(suggestion, element) {
+      if (!element) {
+        this.$nextTick(() => {
+          const overlayElement = this.$refs[this.id].$el.querySelector('.list');
+          const hoveredElement = this.$refs[this.id].$el.querySelectorAll('.list-item')[
+            this.$refs[this.id].hoveredIndex
+          ];
+
+          this.scrollToElement(hoveredElement, overlayElement);
+        });
+      }
+      this.$emit('hover', suggestion, element);
+    },
+    scrollToElement(element, parentElement) {
+      if (parentElement && parentElement.contains(element)) {
+        const { clientHeight, scrollTop } = parentElement;
+        const { offsetHeight, offsetTop } = element;
+
+        if (offsetTop < scrollTop) {
+          // If scrilled down past the last item, scroll all the way to the top
+          if (this.$refs[this.id].hoveredIndex === 0) parentElement.scrollTop = 0;
+          else parentElement.scrollTop = offsetTop; // scroll up
+        } else {
+          const offsetBottom = offsetTop + offsetHeight;
+          const scrollBottom = scrollTop + clientHeight;
+          if (offsetBottom > scrollBottom) {
+            parentElement.scrollTop = offsetBottom - clientHeight; // scroll down
+          }
+        }
+      }
+    },
+    onFocus() {
+      // Select the text inside input to make it easier to clear
+      this.$refs[this.id].$el.querySelector('input').select();
+      // this.processAndShowList()
+    },
+    onChange(suggest) {
+      this.$emit('change', suggest);
+      if (this.clearAfterSelect) {
+        setTimeout(() => {
+          this.onCancel();
+        }, 0);
+      }
+    },
+    onCancel() {
+      this.$emit('change', null);
+      if (this.autocompleteRef) {
+        this.autocompleteRef.clearSelection();
+      }
+
+      // Send the text to vue-simple-suggest as well to trigger a research
+      this.processAndShowList();
+    },
+    showList() {
+      this.autocompleteRef.showList();
+    },
+    hideList() {
+      this.autocompleteRef.hideList();
+    },
+    onInput(text) {
+      // Send the input value to parent. For search etc.
+      this.$emit('update:search-input', text);
+      this.$emit('input', text);
+    },
+    onDropdownIconClick() {
+      // Focus the input element, opening suggestions list and triggering onFocus()
+      this.processAndShowList();
+    },
+    onShowList() {
+      this.$nextTick(() => {
+        this.repositionList();
+      });
+    },
+    repositionList() {
+      console.log('repositionList');
+    },
+    async processAndShowList() {
+      if (this.autocompleteRef) {
+        this.autocompleteRef.inputElement.focus();
+        this.autocompleteRef.showList();
+      }
+    },
+    isScopedSlotEmpty(slot) {
+      if (slot) {
+        const vNode = slot(this);
+        return !(Array.isArray(vNode)
+          || (vNode && (vNode.tag || vNode.context || vNode.text || vNode.children)));
+      }
+      return true;
+    },
+    miscSlotsAreEmpty() {
+      const slots = ['append-item', 'prepend-item'].map((s) => this.$scopedSlots[s]);
+      if (slots.every((s) => !!s)) {
+        return slots.every(this.isScopedSlotEmpty.bind(this));
+      }
+      const slot = slots.find((s) => !!s);
+      return this.isScopedSlotEmpty.call(this, slot);
+    },
+  },
+};
+</script>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,11 +1,125 @@
-// import { shallowMount } from '@vue/test-utils';
-// import main from '../../src/main';
+import { mount } from '@vue/test-utils';
 
-describe('main.js', () => {
-  it('renders props.msg when passed', () => {
-    // const wrapper = shallowMount(main, {
+import AutoComplete from './components/AutoComplete.vue';
 
-    // });
-    expect(true).toBe(true);
+const list = [
+  { displayName: 'first', id: '123' },
+  { displayName: 'second', id: '456' },
+  { displayName: 'third', id: '789' },
+];
+describe('AutoComplete', () => {
+  it('renders the list items', () => {
+    const wrapper = mount(AutoComplete,
+
+      {
+        propsData: {
+          id: 'listId',
+          list,
+          displayAttribute: 'displayName',
+          valueAttribute: 'id',
+        },
+      });
+
+    expect(wrapper.findAll('li').at(0).text()).toBe(list[0].displayName);
+    expect(wrapper.findAll('li').at(1).text()).toBe(list[1].displayName);
+    expect(wrapper.findAll('li').at(2).text()).toBe(list[2].displayName);
+  });
+  it('applies the list item props as html attributes', () => {
+    const wrapper = mount(AutoComplete,
+
+      {
+        propsData: {
+          id: 'listId',
+          list,
+          displayAttribute: 'displayName',
+          valueAttribute: 'id',
+        },
+      });
+    const ComboBlocks = wrapper.findComponent({ name: 'ComboBlocks' });
+    // eslint-disable-next-line no-underscore-dangle
+    const idPrefix = ComboBlocks.vm._uid;
+
+    const item0 = wrapper.findAll('li').at(0);
+    const item1 = wrapper.findAll('li').at(1);
+    const item2 = wrapper.findAll('li').at(2);
+
+    expect(item0.attributes().id).toBe(`${idPrefix}-suggestion-0`);
+    expect(item1.attributes().id).toBe(`${idPrefix}-suggestion-1`);
+    expect(item2.attributes().id).toBe(`${idPrefix}-suggestion-2`);
+
+    expect(item0.attributes().role).toBe('option');
+    expect(item1.attributes().role).toBe('option');
+    expect(item2.attributes().role).toBe('option');
+
+    expect(item0.attributes('aria-selected')).toBe('false');
+    expect(item1.attributes('aria-selected')).toBe('false');
+    expect(item2.attributes('aria-selected')).toBe('false');
+  });
+
+  it('applies the list props as html attributes', () => {
+    const wrapper = mount(AutoComplete,
+
+      {
+        propsData: {
+          id: 'listId',
+          list,
+          displayAttribute: 'displayName',
+          valueAttribute: 'id',
+        },
+      });
+    const ComboBlocks = wrapper.findComponent({ name: 'ComboBlocks' });
+    // eslint-disable-next-line no-underscore-dangle
+    const idPrefix = ComboBlocks.vm._uid;
+
+    const wrapperdList = wrapper.find('ul');
+
+    expect(wrapperdList.attributes().id).toBe(`${idPrefix}-suggestions`);
+    expect(wrapperdList.attributes().role).toBe('listbox');
+    expect(wrapperdList.attributes('aria-labelledby')).toBe(`${idPrefix}-suggestions`);
+  });
+
+  it('applies the input props as html attributes', () => {
+    const wrapper = mount(AutoComplete,
+
+      {
+        propsData: {
+          id: 'listId',
+          list,
+          displayAttribute: 'displayName',
+          valueAttribute: 'id',
+        },
+      });
+    const ComboBlocks = wrapper.findComponent({ name: 'ComboBlocks' });
+    // eslint-disable-next-line no-underscore-dangle
+    const idPrefix = ComboBlocks.vm._uid;
+
+    const input = wrapper.find('input');
+
+    expect(input.attributes('aria-activedescendant')).toBe('');
+    expect(input.attributes('aria-autocomplete')).toBe('list');
+    expect(input.attributes('aria-controls')).toBe(`${idPrefix}-suggestions`);
+  });
+
+  it('applies the combobox props as html attributes', () => {
+    const wrapper = mount(AutoComplete,
+
+      {
+        propsData: {
+          id: 'listId',
+          list,
+          displayAttribute: 'displayName',
+          valueAttribute: 'id',
+        },
+      });
+    const ComboBlocks = wrapper.findComponent({ name: 'ComboBlocks' });
+    // eslint-disable-next-line no-underscore-dangle
+    const idPrefix = ComboBlocks.vm._uid;
+
+    const combobox = wrapper.find('#combobox');
+
+    expect(combobox.attributes('role')).toBe('combobox');
+    expect(combobox.attributes('aria-haspopup')).toBe('listbox');
+    expect(combobox.attributes('aria-expanded')).toBe('false');
+    expect(combobox.attributes('aria-owns')).toBe(`${idPrefix}-suggestions`);
   });
 });
