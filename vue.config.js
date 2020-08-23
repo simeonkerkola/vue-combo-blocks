f95f4d6310bde8cb86d681d2064551b7512ee01f
sssmi <simeon.kerkola@gmail.com>
2020-08-23 22:32:15 +0300

    Change sources

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 console.log('env', process.env.NODE_ENV);
 
 module.exports = {
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'dev/main.js',
+    },
+  },
   css: { extract: false },
   pluginOptions: {
     webpackBundleAnalyzer: {
