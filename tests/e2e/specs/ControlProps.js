e1f6b81bc334631e6f47748d06da26a7affe25cd
Simeon Kerkola <simeon.kerkola@gmail.com>
2021-03-19 13:36:39 +0200

    Watch value prop changes and update internal state accordingly (#18)

---
+++ tests/e2e/specs/ControlProps.js
@@ -0,0 +1,44 @@
+// https://docs.cypress.io/api/introduction/api.html
+const bodyX = 100;
+const bodyY = 300;
+
+describe('Control Props', () => {
+  before(() => {
+    cy.visit('/controlledprop');
+    // can read the initial value
+    cy.findByPlaceholderText(/Search/i)
+      .should('have.value', '');
+  });
+  beforeEach(() => {
+    cy.findByRole('button', { name: /clear/i }).click();
+  });
+
+  it('can read the initial value', () => {
+    cy.findByPlaceholderText(/Search/i)
+      .should('have.value', '');
+
+    cy.findByRole('heading', { name: /Nothing/i });
+  });
+
+  it('can select next item by clicking button', () => {
+    cy.findByRole('button', { name: /select next/i }).click();
+    cy.findByRole('heading', { name: /first/i });
+    cy.findByRole('button', { name: /select next/i }).click();
+    cy.findByRole('heading', { name: /second/i });
+    cy.findByRole('button', { name: /select next/i }).click();
+    cy.findByRole('heading', { name: /third/i });
+    cy.findByRole('button', { name: /select next/i }).click();
+    cy.findByRole('heading', { name: /duplicate/i });
+    cy.findByRole('button', { name: /select next/i }).click();
+    cy.findByRole('heading', { name: /duplicate/i });
+
+    cy.findByRole('button', { name: /select next/i }).click();
+    cy.findByRole('heading', { name: /first/i });
+  });
+
+  it('can select next item normally', () => {
+    cy.findByRole('button', { name: /select next/i }).click();
+    cy.findByPlaceholderText(/Search/i).clear().type('sec{downarrow}{enter}');
+    cy.findByRole('heading', { name: /second/i });
+  });
+});
